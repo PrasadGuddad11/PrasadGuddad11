9be72595f7ccc258114bfe33a0942faa492f1404
Prasad Guddad <prasadguddad11@gmail.com>
2021-09-07 14:52:42 +0000

    cssagain

--- Exam/src/app/app-routing.module.ts
+++ Exam/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddoreditComponent } from './questions/addoredit/addoredit.component';
+import { QualifiersComponent } from './questions/qualifiers/qualifiers.component';
 import { QuestionsComponent } from './questions/questions/questions.component';
 import { WorkspaceComponent } from './questions/workspace/workspace.component';
 import { FlashComponent } from './user/flash/flash.component';
@@ -20,6 +22,12 @@ const routes: Routes = [
   },
   {
     path:"workspace"       ,component:WorkspaceComponent
+  },
+  {
+    path:"addoredit"       ,component:AddoreditComponent
+  },
+  {
+    path:"qualifiers"       ,component:QualifiersComponent
   }
 
 ];
