704aacc5fd917980befa34e80083a9d2f4fbb8dd
Prasad Guddad <prasadguddad11@gmail.com>
2021-09-09 14:37:02 +0000

    done

--- Exam/src/app/questions/addoredit/addoredit.component.ts
+++ Exam/src/app/questions/addoredit/addoredit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const firebase:any;
 @Component({
   selector: 'app-addoredit',
   templateUrl: './addoredit.component.html',
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AddoreditComponent implements OnInit {
 
   constructor() { }
-
+  Qvals:any=[];
+  
   ngOnInit(): void {
+    firebase.database().ref('Questions/').on('value', (snapshot:any) => {
+      var Qvalst:any=[];
+      snapshot.forEach((childSnapshot:any) => {
+        Qvalst.push(childSnapshot.val());
+      });
+      this.Qvals=Qvalst;
+    });
   }
 
 }
